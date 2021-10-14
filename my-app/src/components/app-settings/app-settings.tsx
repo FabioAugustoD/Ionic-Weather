683a7880f02625c09a153c828145e46a2fc71e90
FabioAugustoD <fabioaugustodametto@gmail.com>
2021-10-14 08:54:54 -0300

    Settings page

---
+++ my-app/src/components/app-settings/app-settings.tsx
@@ -0,0 +1,73 @@
+import { Component , State, h} from "@stencil/core";
+
+@Component({
+    tag: "app-settings",
+    styleUrl: "app-settings.css"
+})
+
+export class AppSettings {
+
+    @State() useCurrentLocation: boolean = true;
+    @State() presetLocation: string = "Adelaide";
+    @State() unit: string = "celcius"
+
+    render() {
+        return [
+            <ion-header>
+                <ion-toolbar color="primary">
+                    <ion-buttons slot="start">
+                        <ion-back-button defaultHref="/"/>
+                    </ion-buttons>
+                    <ion-title>Settings</ion-title>
+                </ion-toolbar>
+            </ion-header>,
+
+            <ion-content class="ion-padding">
+                <small>
+                    You may choose to display weather either from your current location,
+                    or a preset location of your choosing.
+                </small>
+                <ion-radio-group value={this.useCurrentLocation ? "current" : "preset"}>
+                    <ion-item>
+                        <ion-label>Use current location</ion-label>
+                        <ion-radio slot="start" value="preset"/>
+                    </ion-item>
+                </ion-radio-group>
+
+                <small>
+                    when using a preset location, the location listyed below
+                    will be used.
+                </small>
+
+                <ion-item>
+                    <ion-input type="text" value={this.presetLocation}></ion-input>
+                </ion-item>
+
+                <small>
+                    Select the unit of measurement that you would like to
+                    use to display
+                    the weather:
+                </small>
+
+                <ion-radio-group value={this.unit}>
+                    <ion-item>
+                        <ion-label>Celcius</ion-label>
+                        <ion-radio value="celcius" slot="start"/>
+                    </ion-item>
+
+                    <ion-item>
+                        <ion-label>Fahrenheit</ion-label>
+                        <ion-radio value="fahrenheit" slot="start"></ion-radio>
+                    </ion-item>
+
+                    <ion-item>
+                        <ion-label>Kelvin</ion-label>
+                        <ion-radio value="kelvin" slot="start"/>
+                    </ion-item>
+                </ion-radio-group>
+                <small hidden={this.unit != "kelvin"}>Kelvin? Seriously?</small>
+            </ion-content>
+        ];
+    }
+
+}
